fb5f5773c8deebf554b6d547bf2c074040e33f89
Tushar Desai <tushardesai@Tushars-MacBook-Pro.local>
2019-11-18 13:53:49 -0500

    reorganized folders into rasberry pi specific folder

--- rasberry_pi_code/voice_commands.py
+++ rasberry_pi_code/voice_commands.py
@@ -133,8 +133,10 @@ def get_googlecal_api_service():
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
+                #Credentials.json is not saved on github, so it must be redownloaded from https://developers.google.com/calendar/quickstart/python for this feature to work
                 flow = InstalledAppFlow.from_client_secrets_file(
                     'credentials.json', SCOPES)
+
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
@@ -931,38 +933,38 @@ class CurrentMarinoCapacity(VoiceCommand):
 
 
 
-class ReadNextXEvents(VoiceCommand):
+# class ReadNextXEvents(VoiceCommand):
 
-    def passes_condition(self, text):
-        return has_x_integers(1, text) and ('what are' in text or 'read' in text) and 'event' in text
+#     def passes_condition(self, text):
+#         return has_x_integers(1, text) and ('what are' in text or 'read' in text) and 'event' in text
         
 
-    def voice_manipulation(self, text):
-        s = text.split()
-        for w in s: 
-            try: 
-                num_events = w2n.word_to_num(w)
-                num_events = int(num_events)
-            except: 
-                pass 
-        self.action(num_events)
+#     def voice_manipulation(self, text):
+#         s = text.split()
+#         for w in s: 
+#             try: 
+#                 num_events = w2n.word_to_num(w)
+#                 num_events = int(num_events)
+#             except: 
+#                 pass 
+#         self.action(num_events)
                 
         
-    def action(self, num_events):
+#     def action(self, num_events):
         
-        service = get_googlecal_api_service()
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                            maxResults=num_events, singleEvents=True,
-                                            orderBy='startTime').execute()
-        events = events_result.get('items', [])
+#         service = get_googlecal_api_service()
+#         # Call the Calendar API
+#         now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+#         events_result = service.events().list(calendarId='primary', timeMin=now,
+#                                             maxResults=num_events, singleEvents=True,
+#                                             orderBy='startTime').execute()
+#         events = events_result.get('items', [])
 
-        if not events:
-            print('No upcoming events found.')
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
+#         if not events:
+#             print('No upcoming events found.')
+#         for event in events:
+#             start = event['start'].get('dateTime', event['start'].get('date'))
+#             print(start, event['summary'])
 
 
         
